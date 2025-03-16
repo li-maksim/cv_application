5be83127d54975c7836738dd3b87c4674845c503
li-maksim <max.mail4@yandex.ru>
2025-03-16 11:00:53 +0300

    Fixed bug where icons and "From"/"to" always appear

--- src/components/CVSection.jsx
+++ src/components/CVSection.jsx
@@ -5,6 +5,38 @@ import { mdiEmailOutline } from '@mdi/js'
 
 function CVSection({generalInfo, schools, jobs, hide}) {
 
+    function Name() {
+        if (generalInfo.firstName && generalInfo.lastName) {
+            return <h2 className="header">{generalInfo.firstName + ' ' + generalInfo.lastName}</h2>
+        } else if (generalInfo.firstName && !generalInfo.lastName) {
+            return <h2 className="header">{generalInfo.firstName}</h2>
+        } else if (!generalInfo.firstName && generalInfo.lastName) {
+            return <h2 className="header">{generalInfo.lastName}</h2>
+        }
+    }
+
+    function Phone() {
+        if (generalInfo.phone) {
+            return (
+                <>
+                    <Icon path={mdiPhone} size={1} className="icon" />
+                    <p>{generalInfo.phone}</p>  
+                </>
+            )
+        }
+    }
+
+    function Email() {
+        if (generalInfo.email) {
+            return (
+                <>
+                    <Icon path={mdiEmailOutline} size={1} className="icon" />
+                    <p>{generalInfo.email}</p>
+                </>
+            )
+        }
+    }
+
     function Schools() {
         if (schools.length > 0) {
             const schoolBlocks = schools.map((item, index) => {
@@ -12,7 +44,10 @@ function CVSection({generalInfo, schools, jobs, hide}) {
                     <div key={item.schoolName + index} className="school_block">
                         <h3>{item.schoolName}</h3>
                         <p>{item.titleOfStudy}</p>
-                        <p>From: {item.studyStart} To: {item.studyEnd}</p>
+                        <p>
+                            {item.studyStart && `From: ${item.studyStart}`}
+                            {item.studyEnd && ` To: ${item.studyEnd}`}
+                        </p>
                     </div>
                 )
             })
@@ -33,7 +68,10 @@ function CVSection({generalInfo, schools, jobs, hide}) {
                     <div key={item.schoolName + index} className="school_block">
                         <h3>{item.companyName}</h3>
                         <p>{item.positionTitle}</p>
-                        <p>From: {item.jobStart} to{item.jobEnd}</p>
+                        <p>
+                            {item.jobStart && `From: ${item.jobStart}`}
+                            {item.jobEnd && ` To: ${item.jobEnd}`}
+                        </p>
                     </div>
                 )
             })
@@ -58,29 +96,13 @@ function CVSection({generalInfo, schools, jobs, hide}) {
         }
     }
 
-    function Name() {
-        if (generalInfo.firstName && generalInfo.lastName) {
-            return <h2 className="header">{generalInfo.firstName + ' ' + generalInfo.lastName}</h2>
-        } else if (generalInfo.firstName && !generalInfo.lastName) {
-            return <h2 className="header">{generalInfo.firstName}</h2>
-        } else if (!generalInfo.firstName && generalInfo.lastName) {
-            return <h2 className="header">{generalInfo.lastName}</h2>
-        }
-    }
-
     return (
         <section className={hide}>
         <div className="general_info">
             <Name />
             <h3 className="header">{generalInfo.jobTitle}</h3>
-            <div className="contact">
-                <Icon path={mdiPhone} size={1} className="icon" />
-                <p>{generalInfo.phone}</p>
-            </div>
-            <div className="contact">
-                <Icon path={mdiEmailOutline} size={1} className="icon" />
-                <p>{generalInfo.email}</p>
-            </div>
+            <div className="contact"><Phone /></div>
+            <div className="contact"><Email /></div>
         </div>
         <div className="other_info">
             <div className="other_info_block"><Schools /></div>
